090d5745d4894163147b8db1a6b1f3cece93dfbd
Fatass <main@Fatasss-MacBook-Pro.local>
2015-03-26 20:24:48 -0500

    Initial Commit

---
+++ UT Study Spots/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UT Study Spots
+//
+//  Created by Fatass on 3/26/15.
+//  Copyright (c) 2015 Fatass. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ UT Study SpotsTests/UT_Study_SpotsTests.m
@@ -0,0 +1,40 @@
+//
+//  UT_Study_SpotsTests.m
+//  UT Study SpotsTests
+//
+//  Created by Fatass on 3/26/15.
+//  Copyright (c) 2015 Fatass. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface UT_Study_SpotsTests : XCTestCase
+
+@end
+
+@implementation UT_Study_SpotsTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
