b24c1ab92b89c015076bd6a1038573ac3ae36b4d
Fatass <main@Fatasss-MacBook-Pro.local>
2015-04-01 00:40:13 -0500

    initcommit remote

---
+++ UT Study Spots/LoadViewController.h
@@ -0,0 +1,26 @@
+//
+//  LoadViewController.h
+//  UT Study Spots
+//
+//  Created by Fatass on 4/1/15.
+//  Copyright (c) 2015 Fatass. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
+
+@interface LoadViewController : UIViewController
+
+
+
+@end
+
+
+
+
+
+
+
+
+
+
---
+++ UT Study Spots/LoadViewController.m
@@ -0,0 +1,29 @@
+//
+//  LoadViewController.m
+//  UT Study Spots
+//
+//  Created by Fatass on 4/1/15.
+//  Copyright (c) 2015 Fatass. All rights reserved.
+//
+
+#import "LoadViewController.h"
+
+@interface LoadViewController ()
+
+@end
+
+@implementation LoadViewController
+
+
+
+@end
+
+
+
+
+
+
+
+
+
+
